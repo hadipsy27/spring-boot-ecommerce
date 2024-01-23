cfb18d24c9406ad37e06abeb06977dc361afa61e
Hadi Prasetyo <33802468+hadipsy27@users.noreply.github.com>
2024-01-23 23:56:39 +0700

    Search for Products by Category - Code - Update ProductService

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,26 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../../services/product.service';
 import { Product } from '../../common/product';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list-grid.component.html',
-  // templateUrl: './product-list-table.component.html',
-  // templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.css'
 })
 export class ProductListComponent {
 
   products: Product[] = [];
+  currentCategoryId: number = 1;
 
-  constructor(private productService: ProductService){}
+  constructor(private productService: ProductService, private route: ActivatedRoute){}
 
   ngOnInit(): void{
-    this.listProducts();
+    this.route.paramMap.subscribe(() => {
+      this.listProducts();
+    })
   }
 
   listProducts() {
-    this.productService.getProductList().subscribe(
+
+    // Retrieve the 'id' parameter from the route
+    const categoryIdParam = this.route.snapshot.paramMap.get('id');
+
+    // Check if categoryIdParam is not null or undefined
+    if (categoryIdParam !== null && categoryIdParam !== undefined) {
+      // Convert the categoryIdParam to a number using the "+" symbol
+      this.currentCategoryId = +categoryIdParam;
+    } else {
+      // If 'id' parameter is not found, default to category id 1
+      this.currentCategoryId = 1;
+    }
+    
+    this.productService.getProductList(this.currentCategoryId).subscribe(
       data => {
         this.products = data;
       }
