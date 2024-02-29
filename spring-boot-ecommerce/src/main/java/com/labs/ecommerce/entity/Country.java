009914992c999509af355f4b7ee1d39d94e7e657
Hadi Prasetyo <33802468+hadipsy27@users.noreply.github.com>
2024-02-29 21:59:13 +0700

    create Entity Country have relation with State

---
+++ spring-boot-ecommerce/src/main/java/com/labs/ecommerce/entity/Country.java
@@ -0,0 +1,30 @@
+package com.labs.ecommerce.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Table(name = "country")
+@Getter
+@Setter
+public class Country {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "code")
+    private String code;
+
+    @Column(name = "name")
+    private String name;
+
+    // TODO: Set up many-to-many with states
+
+    @OneToMany(mappedBy = "country")
+    private List<State> states;
+}
