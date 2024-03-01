b06525170b5323e934a9ec40f7e349c36d02c375
Hadi Prasetyo <33802468+hadipsy27@users.noreply.github.com>
2024-03-01 15:00:08 +0700

    200. eCommerce Project - Checkout Form - Save the Order to Database - Entities - 1 - entity Customer & Address

---
+++ spring-boot-ecommerce/src/main/java/com/labs/ecommerce/entity/Address.java
@@ -0,0 +1,33 @@
+package com.labs.ecommerce.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "address")
+@Getter
+@Setter
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "city")
+    private String city;
+
+    @Column(name = "contry")
+    private String contry;
+
+    @Column(name = "state")
+    private String state;
+
+    @Column(name = "street")
+    private String street;
+
+    @Column(name = "zip_code")
+    private String zipCode;
+
+}
---
+++ spring-boot-ecommerce/src/main/java/com/labs/ecommerce/entity/Customer.java
@@ -0,0 +1,27 @@
+package com.labs.ecommerce.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "customer")
+@Getter
+@Setter
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+
+}
