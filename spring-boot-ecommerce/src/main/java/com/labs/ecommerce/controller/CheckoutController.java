aa3dea0b06d1d6a9af5b8bee07f062cda095a3a7
Hadi Prasetyo <33802468+hadipsy27@users.noreply.github.com>
2024-03-02 10:56:12 +0700

    208. eCommerce Project - Checkout Form - Save the Order to Database - REST Controller

---
+++ spring-boot-ecommerce/src/main/java/com/labs/ecommerce/controller/CheckoutController.java
@@ -0,0 +1,27 @@
+package com.labs.ecommerce.controller;
+
+import com.labs.ecommerce.dto.Purchase;
+import com.labs.ecommerce.dto.PurchaseResponse;
+import com.labs.ecommerce.service.CheckoutService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin("http://localhost:4200")
+@RestController
+@RequestMapping("/api/checkout")
+public class CheckoutController {
+
+    private CheckoutService checkoutService;
+
+    @Autowired
+    public CheckoutController(CheckoutService checkoutService) {
+        this.checkoutService = checkoutService;
+    }
+
+
+    @PostMapping("/purchase")
+    public PurchaseResponse placeOrder(@RequestBody Purchase purchase){
+        PurchaseResponse purchaseResponse = checkoutService.placeOrder(purchase);
+        return purchaseResponse;
+    }
+}
