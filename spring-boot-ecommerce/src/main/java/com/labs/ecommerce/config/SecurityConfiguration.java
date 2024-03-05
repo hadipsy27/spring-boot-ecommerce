24bcef12ca2865e0ff7537ef7453d24000d9c279
Hadi Prasetyo <33802468+hadipsy27@users.noreply.github.com>
2024-03-05 11:57:24 +0700

    267. & 268 Release 3.0 - Security - Order History - Secure Backend - Protect Endpoints

---
+++ spring-boot-ecommerce/src/main/java/com/labs/ecommerce/config/SecurityConfiguration.java
@@ -0,0 +1,43 @@
+package com.labs.ecommerce.config;
+
+import com.okta.spring.boot.oauth.Okta;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.accept.ContentNegotiationStrategy;
+import org.springframework.web.accept.HeaderContentNegotiationStrategy;
+
+@Configuration
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        //   Protect endpoint /api/orders
+        http.authorizeRequests()
+                .requestMatchers("/api/orders/**")
+                .authenticated()
+                .anyRequest()
+                .permitAll();
+
+
+        // Add CORS filter
+        http.cors(Customizer.withDefaults());
+
+        // + content negotiation strategy
+        http.setSharedObject(ContentNegotiationStrategy.class, new HeaderContentNegotiationStrategy());
+
+        // + non-empty response body for 401 (more friendly)
+        //  Okta.configureResourceServer401ResponseBody(http);
+
+        // we are not using Cookies for session tracking >> disable CSRF
+        http.csrf(AbstractHttpConfigurer::disable);
+
+        return http.build();
+
+    }
+}
