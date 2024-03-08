10e605570f425e6c2aae671021b41ee766122b20
Hadi Prasetyo <33802468+hadipsy27@users.noreply.github.com>
2024-03-08 22:37:10 +0700

    301. Payment Processing - Stripe - Backend Coding - Checkout Service

--- spring-boot-ecommerce/src/main/java/com/labs/ecommerce/service/CheckoutService.java
+++ spring-boot-ecommerce/src/main/java/com/labs/ecommerce/service/CheckoutService.java
@@ -1,9 +1,13 @@
 package com.labs.ecommerce.service;
 
+import com.labs.ecommerce.dto.PaymentInfo;
 import com.labs.ecommerce.dto.Purchase;
 import com.labs.ecommerce.dto.PurchaseResponse;
+import com.stripe.exception.StripeException;
+import com.stripe.model.PaymentIntent;
 
 public interface CheckoutService {
 
     PurchaseResponse placeOrder(Purchase purchase);
+    PaymentIntent createPaymentIntent(PaymentInfo paymentInfo) throws StripeException;
 }
--- spring-boot-ecommerce/src/main/java/com/labs/ecommerce/service/CheckoutServiceImpl.java
+++ spring-boot-ecommerce/src/main/java/com/labs/ecommerce/service/CheckoutServiceImpl.java
@@ -1,17 +1,21 @@
 package com.labs.ecommerce.service;
 
 import com.labs.ecommerce.dao.CustomerRepository;
+import com.labs.ecommerce.dto.PaymentInfo;
 import com.labs.ecommerce.dto.Purchase;
 import com.labs.ecommerce.dto.PurchaseResponse;
 import com.labs.ecommerce.entity.Customer;
 import com.labs.ecommerce.entity.Order;
 import com.labs.ecommerce.entity.OrderItem;
+import com.stripe.Stripe;
+import com.stripe.exception.StripeException;
+import com.stripe.model.PaymentIntent;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import java.util.Set;
-import java.util.UUID;
+import java.util.*;
 
 @Service
 public class CheckoutServiceImpl implements CheckoutService{
@@ -19,8 +23,11 @@ public class CheckoutServiceImpl implements CheckoutService{
     private CustomerRepository customerRepository;
 
     @Autowired
-    public CheckoutServiceImpl(CustomerRepository customerRepository) {
+    public CheckoutServiceImpl(CustomerRepository customerRepository, @Value("${stripe.key.secret}") String secretKey) {
         this.customerRepository = customerRepository;
+
+        // Initialize Stripe key API with secret key
+        Stripe.apiKey = secretKey;
     }
 
     @Override
@@ -56,6 +63,19 @@ public class CheckoutServiceImpl implements CheckoutService{
 
         // Return a response
         return new PurchaseResponse(orderTrackingNumber);
+    }
+
+    @Override
+    public PaymentIntent createPaymentIntent(PaymentInfo paymentInfo) throws StripeException {
+        List<String> paymentMethodTypes = new ArrayList<>();
+        paymentMethodTypes.add("card");
+
+        Map<String, Object> params = new HashMap<>();
+        params.put("amount", paymentInfo.getAmount());
+        params.put("currency", paymentInfo.getCurrency());
+        params.put("payment_method_types", paymentMethodTypes);
+
+        return PaymentIntent.create(params);
     }
 
     private String generateOrderTrackingNumber() {
